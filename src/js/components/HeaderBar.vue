bcc2065b46dd5fe8038197a4e9f86a998ae9c222
demonic <ian910297@gmail.com>
2016-12-13 21:30:23 -0800

    add slide-nav

--- src/js/components/HeaderBar.vue
+++ src/js/components/HeaderBar.vue
@@ -2,14 +2,31 @@
 </style>
 
 <template>
-  <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Demonic</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
+  <v-nav>
+    <a href="#!" class="brand-logo" slot="logo">Demonic</a>
+    <a v-side-nav:mobile_nav="nav" class="button-collapse hide-on-large-only"><v-icon medium>list</v-icon></a>
+    <ul class="right hide-on-med-and-down">
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/experience">Experience</router-link></li>
+    </ul>
+    <v-side-nav id="mobile_nav">
+      <ul>
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/experience">Experience</router-link></li>
       </ul>
-    </div>
-  </nav>
+    </v-side-nav>
+  </v-nav>
 </template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      nav: {
+        edge: 'right'
+      }
+    }
+  }
+}
+</script>
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueMaterial from 'vue-materials'
+
 import App from './views/App'
 import routes from './routes'
 
+Vue.use(VueMaterial)
 Vue.use(VueRouter)
+
 const router = new VueRouter({
   routes: routes,
   mode: 'history'
