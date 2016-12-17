fb1d55d7e321442828b082fac9cc86c9d7f7e47b
demonic <ian910297@gmail.com>
2016-12-17 08:23:49 -0800

    use normal and simple format

--- AbandonTreatment/vue_version_site/src/js/components/HeaderBar.vue
+++ AbandonTreatment/vue_version_site/src/js/components/HeaderBar.vue
@@ -4,7 +4,6 @@
 }
 .nav-wrapper {
   background: white;
-
   a {
     color: black;
   }
---
+++ js/common.js
@@ -0,0 +1,7 @@
+$(function() {
+  var current_year = new Date().getFullYear();
+  $('#current_year').html(current_year);
+
+  $('#nav-mobile').sideNav();
+});
+
